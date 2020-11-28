2bbd51ddde99325b3052553d35b0e8fe4667d18b
ShoooGit <shooo.ity.777@gmail.com>
2020-11-28 11:58:26 +0900

    with内でのループについて

--- with.py
+++ with.py
@@ -1,6 +1,4 @@
-# ここから下に書き写してください
-with open('read.txt', encoding='utf-8') as f:
-    # 開いた内容を読み込む
-    contents = f.read()
-    # 表示する
-    print(contents)
+# ファイルinput/telephone.csvの内容を表示しましょう。
+with open('input/telephone.csv', encoding='utf-8') as f:
+    for row in f:
+        print(row.rstrip())

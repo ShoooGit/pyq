8f3ec873c108176f94dd418a5760441f0b504c04
ShoooGit <shooo.ity.777@gmail.com>
2020-11-28 11:38:40 +0900

    アイテムの追加

--- value-and-key.py
+++ value-and-key.py
@@ -3,7 +3,10 @@ health_result = {'名前': '佐藤', '年齢': 35, '身長': 170, '体重': 60, 
 # 値のリストを表示
 health_result_keys = health_result.keys()
 health_result_values = health_result.values()
+# キーと値の(key, value)対のリストを表示
+health_result_items = health_result.items()
 
 # 表示
 print(health_result_keys)
 print(health_result_values)
+print(health_result_items)

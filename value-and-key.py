448f7ead6fbf1bf98c8aabd33d7933388e424a19
ShoooGit <shooo.ity.777@gmail.com>
2020-11-28 11:36:52 +0900

    キーとバリューについて

---
+++ value-and-key.py
@@ -0,0 +1,9 @@
+health_result = {'名前': '佐藤', '年齢': 35, '身長': 170, '体重': 60, '視力': 2.0}
+
+# 値のリストを表示
+health_result_keys = health_result.keys()
+health_result_values = health_result.values()
+
+# 表示
+print(health_result_keys)
+print(health_result_values)

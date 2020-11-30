c24fd08d34f9b3d3321797f360c9fe2a67a6149a
ShoooGit <shooo.ity.777@gmail.com>
2020-11-30 22:12:16 +0900

    集合演算について

--- set.py
+++ set.py
@@ -1,18 +1,31 @@
 def main():
-    set_example_1 = {1, 2, 2, 3}
-    print(set_example_1)
+    # 和集合を求めましょう
+    union = {1, 2, 3} | {2, 3, 4}
+    print(union)
 
-    list_example = [1, 1, 2, 3]
-    set_example_2 = set(list_example)
-    print(set_example_2)
+    # 積集合を求めましょう
+    intersection = {1, 2, 3} & {2, 3, 4}
+    print(intersection)
 
-    tuple_example = ('A', 'B', 'C', 'C')
-    set_example_3 = set(tuple_example)
-    print(set_example_3)
+    # 差集合を求めましょう
+    difference = {1, 2, 3} - {2, 3, 4}
+    print(difference)
 
-    empty_set = set()
-    print(empty_set)
+    # 2つの集合の対称差
+    sym_diff = {1, 2, 3} ^ {2, 3, 4}
+    print(sym_diff)
 
+    # 左辺の要素全てが右辺の集合に含まれている場合
+    subset_1 = {1, 3} <= {1, 2, 3}
+    print(subset_1)
 
-if __name__ == "__main__":
+    # 右辺の要素全てが左辺の集合に含まれている場合
+    subset_2 = {4, 5, 6} >= {4, 5}
+    print(subset_2)
+
+    # 左辺に右辺の集合に存在しない要素が含まれている場合
+    subset_3 = {3, 4} <= {1, 2, 3}
+    print(subset_3)
+
+if __name__ == '__main__':
     main()

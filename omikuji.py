b5fe59da9dacbbc786362c3794f0216234563556
ShoooGit <shooo.ity.777@gmail.com>
2020-11-30 12:17:19 +0900

    おみくじ

---
+++ omikuji.py
@@ -0,0 +1,28 @@
+import random
+
+fortunes = {
+    1: '凶',
+    2: '末吉',
+    3: '小吉',
+    4: '中吉',
+    5: '吉',
+    6: '大吉'
+}
+
+# おみくじの番号(1~6)をランダムに決定
+number = random.randint(1, 6)
+# 辞書fortunesから番号に対応する運勢を取得
+fortune = fortunes[number]
+# 読み込むファイル名を作成
+file_name = 'input/fortune_' + str(number) + '.txt'
+
+# ファイルを読み込んで、その中からランダムな1行をメッセージとして取得する
+with open(file_name, encoding='utf-8') as f:
+    messages = f.readlines()
+    message = random.choice(messages)
+
+print('あなたの運勢は...')
+# 運勢を表示
+print(fortune)
+# メッセージを表示
+print(message)
--- random.py
+++
@@ -1,9 +0,0 @@
-import random
-# ベットする数
-user_selected = 8
-row = 1
-
-for num in range(1,101):
-    if user_selected == random.randint(1,10):
-        print(row, "回目に当たり")
-    row += 1

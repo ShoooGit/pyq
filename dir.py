b8a35a3362fa7bb3d77b15475476063e97e18c34
ShoooGit <shooo.ity.777@gmail.com>
2020-12-06 18:06:19 +0900

    子ディレクトリも走査の対象とした

--- dir.py
+++ dir.py
@@ -3,9 +3,9 @@ import os
 
 
 def search(target_path, search_text):
-    for filename in os.listdir(target_path):
-        file_path = os.path.join(target_path, filename)
-        if os.path.isfile(file_path):
+    for path, _, filenames in os.walk(target_path):
+        for filename in filenames:
+            file_path = os.path.join(path, filename)
             with open(file_path, encoding='utf-8') as f:
                 if search_text in f.read():
                     print(filename)
